2bbba0a279d1045c1afe09dd246c9bdbbf6db449
thaReal <daniel.ruland@gmail.com>
2016-09-05 20:54:04 -0400

    working besides every other retarget off by 0.00000064

--- src/main.cpp
+++ src/main.cpp
@@ -1097,7 +1097,7 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
     // this calculates block reward truncated to two decimal points
     double dParam = 2.45 - (nMonths * 0.01);
     double dMultiplier = exp(dParam);
-    float fTruncatedMultiplier = (floor(dMultiplier*100)) / 100;
+    float fTruncatedMultiplier = floor(dMultiplier * 100) / 100);
 
     int64 nSubsidy = fTruncatedMultiplier * COIN;
     return nSubsidy + nFees;
